c0b3ec8480cdcce76380381849c887ed80680505
YudaiYaguchi <yudai.yaguchi0503@gmail.com>
2025-03-10 22:20:47 +0900

    fix:リンクの入れ子をなくしエラーを解消する

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -12,92 +12,65 @@ type LayoutProps = {
 export const Layout: FC<LayoutProps> = ({ userName }) => {
   return (
     <>
-      <Box as="header" w='full'>
+      <Box as="header" w="full">
         <Flex
           background="linear-gradient(135deg, rgb(63, 63, 204), rgb(120, 120, 255))"
           align="center"
           py={4}
           px={4}
         >
-          <Link to="/home">
-            <Heading as="h1" color="white" fontFamily="Oswald, sans-serif" fontSize='2xl' fontWeight="700" cursor="pointer" pl={6}>
+          <ChakraLink as={Link} to="/home" _hover={{ textDecoration: "none" }}>
+            <Heading as="h1" color="white" fontFamily="Oswald, sans-serif" fontSize="2xl" fontWeight="700" cursor="pointer" pl={6}>
               就活管理 ~JobMate~
             </Heading>
-          </Link>
-          {!userName && (
-            <>
-              <HStack position='absolute' right='0px' padding='8px'>
-                <Button bg='none' border='none' color="white">
-                  ログイン
-                </Button>
-                <Button bg='white' color='blue'>
-                  会員登録
-                </Button>
-              </HStack>
-            </>
-          )
-          }
-          {userName && (
-            <HStack position='absolute' right='0px' padding='12px' gap="12px" >
+          </ChakraLink>
+
+          {!userName ? (
+            <HStack position="absolute" right="0px" padding="8px">
+              <Button as={Link} to="/login" bg="none" border="none" color="white">
+                ログイン
+              </Button>
+              <Button as={Link} to="/register" bg="white" color="blue">
+                会員登録
+              </Button>
+            </HStack>
+          ) : (
+            <HStack position="absolute" right="0px" padding="12px" gap="12px">
               <FaBell cursor="pointer" />
               <HStack>
-                <FaUser /><Text>{userName}</Text>
+                <FaUser />
+                <Text>{userName}</Text>
               </HStack>
             </HStack>
           )}
         </Flex>
+
         {userName && (
-          <>
-            <Flex
-              justify="center"
-              w="60%"
-              py={2}
-              gap={5}
-              borderBottom="2px solid #ddd"
-              mx="auto"
-              fontFamily="bold"
-            >
-              <Link to="/home">
-                <ChakraLink
-                  textDecoration="none"
-                  color="blue.500"
-                  _hover={{ textDecoration: "underline" }}
-                  display="flex"
-                  alignItems="center"
-                >
-                  <IoHomeOutline />
-                </ChakraLink>
-              </Link>
+          <Flex
+            justify="center"
+            w="60%"
+            py={2}
+            gap={5}
+            borderBottom="2px solid #ddd"
+            mx="auto"
+            fontWeight="bold"
+          >
+            <ChakraLink as={Link} to="/home" color="blue.500" _hover={{ textDecoration: "underline" }} display="flex" alignItems="center">
+              <IoHomeOutline />
+            </ChakraLink>
 
-              <Link to="/all-es">
-                <ChakraLink
-                  textDecoration="none"
-                  color="blue.500"
-                  _hover={{ textDecoration: "underline" }}
-                  display="flex"
-                  alignItems="center"
-                >
-                  ESまとめ
-                </ChakraLink>
-              </Link>
+            <ChakraLink as={Link} to="/all-es" color="blue.500" _hover={{ textDecoration: "underline" }} display="flex" alignItems="center">
+              ESまとめ
+            </ChakraLink>
 
-              <Link to="/interview-questions">
-                <ChakraLink
-                  textDecoration="none"
-                  color="blue.500"
-                  _hover={{ textDecoration: "underline" }}
-                  display="flex"
-                  alignItems="center"
-                >
-                  面接質問まとめ
-                </ChakraLink>
-              </Link>
-            </Flex>
-          </>
+            <ChakraLink as={Link} to="/interview-questions" color="blue.500" _hover={{ textDecoration: "underline" }} display="flex" alignItems="center">
+              面接質問まとめ
+            </ChakraLink>
+          </Flex>
         )}
-      </Box >
+      </Box>
 
-      <Outlet /> {/*ここの部分が変わる*/}
+      <Outlet />
 
       <Box
         as="footer"
@@ -116,4 +89,3 @@ export const Layout: FC<LayoutProps> = ({ userName }) => {
     </>
   );
 };
-
