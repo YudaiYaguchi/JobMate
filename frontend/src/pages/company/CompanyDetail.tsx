00ab3ac6f3d4f786bc587774a827ffc2ba3f724c
YudaiYaguchi <yudai.yaguchi0503@gmail.com>
2025-03-16 21:39:28 +0900

    feat:企業詳細表示カードの追加

---
+++ frontend/src/pages/company/CompanyDetail.tsx
@@ -0,0 +1,32 @@
+import { Company } from "../../types/Company";
+import { Text, Box, Badge, HStack } from "@chakra-ui/react";
+import { FC } from "react";
+
+type CompanyDetailProps = {
+  company: Company;
+}
+
+const CompanyDetail: FC<CompanyDetailProps> = ({ company }) => {
+  return (
+    <Box
+      flex="1"
+      p={4}
+      borderWidth={1}
+      borderRadius="md"
+      bg="gray.50"
+      boxShadow="md"
+    >
+      <HStack justify="space-between" mb={2}>
+        <Badge colorScheme="blue" fontSize="lg">{company.selection_type}</Badge>
+        <Badge colorScheme={company.selection_status === "進行中" ? "yellow" : "green"}>
+          {company.selection_status}
+        </Badge>
+      </HStack>
+      <Text mb={1}><strong>選考日付:</strong> {company.selection_date}</Text>
+      <Text><strong>選考結果:</strong> {company.selection_result}</Text>
+      <Text><strong>マイページURL:</strong> </Text>
+    </Box>
+  );
+};
+
+export default CompanyDetail;
